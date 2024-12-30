c91a6034fa247dfbdaa6b2028fa2799ed241c2af
roshdyjr <ahmedrush96@gmail.com>
2024-12-30 16:15:45 +0200

    logo test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
             <header className='absolute top-1/2 w-full -translate-y-1/2'>
                 <nav className='flex size-full items-center justify-between p-4'>
                     <div className='flex items-center gap-7'>
-                        <img src='/img/logo.png' alt='logo' className='w-10' />
+                        <img src='/img/logo.png' alt='logo' className='w-16' />
                         <Button id="portfolio-button" title="Portfolio" rightIcon={<TiLocationArrow />} containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1" />
                     </div>
 
